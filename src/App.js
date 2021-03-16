0eddf2f79e472dbd84cd51fcc5a03c3eb6babb9d
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-15 18:40:40 -0700

    created helper folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as ROUTES from "./constants/routes";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ScrollToTop from "./scrollToTop";
+import ScrollToTop from "./helpers/scrollToTop";
 import {
   HomePage,
   FeaturedPage,
